15bb771620e001a9e16d1ed662ee959893a37f3d
Anuj Hooda <hoodaanuj03@gmail.com>
2026-04-28 10:20:16 +0530

    Feat: fixing errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
+
 import "./App.css";
-import MainPage from "./Components/MainPage";
+import MainPage from "./Page/MainPage";
+
 
 function App() {
   return (
